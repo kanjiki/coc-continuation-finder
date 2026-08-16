14ca62ca3baa660021ae77ed91eca566dfa80e81
kanjiki <kota.ayaki@gmail.com>
2026-08-16 23:09:20 +0900

    Connect candidate requests to GAS

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.COC_CONFIG = {
-  candidateRequestEndpoint: ''
+  candidateRequestEndpoint: 'https://script.google.com/macros/s/AKfycbwy7AqfUP9gQRi5gjA67c17Xd96rWeo1ptEp1-qZOmapWxqlhstSKYrRKWJJrCA-QU-VA/exec'
 };
