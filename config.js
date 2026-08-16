7f90a80334270a21ec38ca50aa1a7786e44fdbb0
kanjiki <kota.ayaki@gmail.com>
2026-08-16 23:40:21 +0900

    Pause public submissions until Apps Script allows anonymous access

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.COC_CONFIG = {
-  candidateRequestEndpoint: 'https://script.google.com/macros/s/AKfycbwy7AqfUP9gQRi5gjA67c17Xd96rWeo1ptEp1-qZOmapWxqlhstSKYrRKWJJrCA-QU-VA/exec',
-  recommendationFeedbackEnabled: true
+  candidateRequestEndpoint: '',
+  recommendationFeedbackEnabled: false
 };
